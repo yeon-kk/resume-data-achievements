c5d3f9caeefe054330f40182209446fe0ba6d09f
yeonkyoung <keep.ykk@gmail.com>
2023-04-05 00:21:20 +0900

    [#9] Redirect 페이지 추가 (#10)
    * Feat: redirect 가능하도록 router에 페이지 추가
    
    * Feat: Redirect 페이지 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,11 +1,16 @@
 import { Navigate, createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import Redirect from '../pages/Redirect';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <div />,
+  },
+  {
+    path: '/redirect',
+    element: <Redirect />,
   },
   {
     path: '*',
