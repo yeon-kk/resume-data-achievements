6363d23673f5cb0883a2673fab5e560e40ab6c21
yeonkyoung <keep.ykk@gmail.com>
2023-04-05 00:29:37 +0900

    Feat: 글 공개 단계를 선택하는 checkbox 컴포넌트 생성

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,31 @@
+import { Checkbox, CheckboxGroup, Stack } from '@chakra-ui/react';
+
+interface CheckBoxPropsType {
+  values: string[];
+  isChecked: boolean[];
+  size: string;
+}
+
+function CheckBox({ values, isChecked, size }: CheckBoxPropsType) {
+  const onChangeHandler = (index: number) => () => {
+    isChecked[index] = !isChecked[index];
+  };
+
+  return (
+    <CheckboxGroup colorScheme="green" defaultValue={values}>
+      <Stack spacing={[1, 5]} direction={'row'}>
+        {values.map((value, index) => (
+          <Checkbox
+            onChange={onChangeHandler(index)}
+            key={`checkbox-${value}`}
+            value={value}
+            size={size}
+          >
+            {value}
+          </Checkbox>
+        ))}
+      </Stack>
+    </CheckboxGroup>
+  );
+}
+export default CheckBox;
